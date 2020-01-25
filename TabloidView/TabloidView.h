6af9ace5c1ec27b02803ef6e2c1bc5f43e5e426f
Alexander Kadyrov <alexander-kad@yandex.ru>
2020-01-25 15:56:36 +0300

    Initial Commit

---
+++ TabloidView/TabloidView.h
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double TabloidViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char TabloidViewVersionString[];
---
+++ TabloidViewTests/TabloidViewTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import TabloidView
+
+class TabloidViewTests: XCTestCase {
+    func testExample() {
+        
+    }
+}
