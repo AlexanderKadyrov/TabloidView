19ae6e3a273674f1a2d9ee2d512b9363e02d129b
Alexander Kadyrov <alexander-kad@yandex.ru>
2020-04-26 02:54:46 +0300

    remove file

--- TabloidView/Sources/Classes/Views/TabloidView/Protocols/TabloidCellViewProtocol.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-public protocol TabloidCellViewProtocol: UITableViewCell {
-    
-    // MARK: - Properties
-    
-    associatedtype TabloidCellViewModelProtocol
-    var viewModel: TabloidCellViewModelProtocol! { get set }
-}
