2e19616fb68a78c8d09f68e83595da6af6ccb5d9
Alexander Kadyrov <alexander-kad@yandex.ru>
2020-04-25 23:37:07 +0300

    work with TabloidCellView.swift

--- TabloidView/Sources/Classes/Views/TabloidView/TabloidViewCell/TabloidCellView.swift
+++ TabloidView/Sources/Classes/Views/TabloidView/TabloidViewCell/TabloidCellView.swift
@@ -6,18 +6,6 @@ open class TabloidCellView: UITableViewCell {
     
     open var viewModel: TabloidCellViewModel!
     
-    // MARK: - Initialization
-    
-    public required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.backgroundColor = .clear
-        backgroundColor = .clear
-    }
-    
     // MARK: - Height
     
     @objc open class func height(viewModel: TabloidCellViewModel) -> NSNumber {
