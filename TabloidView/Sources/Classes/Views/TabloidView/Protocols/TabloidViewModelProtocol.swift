59bca5a4c7686860843b40d6ed8501211fc4b323
Alexander Kadyrov <alexander-kad@yandex.ru>
2020-04-25 23:51:50 +0300

    work with TabloidViewModelProtocol.swift

--- TabloidView/Sources/Classes/Views/TabloidView/Protocols/TabloidViewModelProtocol.swift
+++ TabloidView/Sources/Classes/Views/TabloidView/Protocols/TabloidViewModelProtocol.swift
@@ -2,4 +2,11 @@ import Foundation
 
 public protocol TabloidViewModelProtocol {
     
+    // MARK: - Properties
+    
+    var cellIdentifiers: [String] { get }
+    
+    // MARK: - Initialization
+    
+    init(cellIdentifiers: [String])
 }
