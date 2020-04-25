f4a89dfb58372403cbc1563fb007304a72dfed35
Alexander Kadyrov <alexander-kad@yandex.ru>
2020-04-25 23:12:03 +0300

    work with TabloidViewModel.swift

--- TabloidView/Sources/Classes/Views/TabloidView/TabloidViewModel.swift
+++ TabloidView/Sources/Classes/Views/TabloidView/TabloidViewModel.swift
@@ -3,32 +3,16 @@ import Foundation
 
 open class TabloidViewModel {
     
-    // MARK: - Enums
-    
-    public enum SeparatorStyle {
-        case singleLine
-        case none
-    }
-    
-    public enum TabloidStyle {
-        case grouped
-        case plain
-    }
-    
     // MARK: - Properties
     
     public let pipeDidSelectItem = Signal<TabloidCellViewModel, Never>.pipe()
     public let elements = MutableProperty<[[TabloidCellViewModel]]>([])
     
-    public let separatorStyle: SeparatorStyle
-    public let tabloidStyle: TabloidStyle
     public let cellIdentifiers: [String]
     
     // MARK: - Initialization
     
-    public init(tabloidStyle: TabloidStyle, separatorStyle: SeparatorStyle, cellIdentifiers: [String]) {
+    public init(cellIdentifiers: [String]) {
         self.cellIdentifiers = cellIdentifiers
-        self.separatorStyle = separatorStyle
-        self.tabloidStyle = tabloidStyle
     }
 }
