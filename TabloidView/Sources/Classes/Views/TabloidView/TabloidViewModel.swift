c0f7321f9f5a06c039b2ef4069c8dc2309cface9
Alexander Kadyrov <alexander-kad@yandex.ru>
2020-04-26 02:56:58 +0300

    work with TabloidViewModel.swift

--- TabloidView/Sources/Classes/Views/TabloidView/TabloidViewModel.swift
+++ TabloidView/Sources/Classes/Views/TabloidView/TabloidViewModel.swift
@@ -1,6 +1,20 @@
 import ReactiveSwift
 import Foundation
 
+public protocol TabloidViewModelProtocol {
+    
+    // MARK: - Properties
+    
+    var pipeDidSelectItem: (output: Signal<TabloidCellViewModelProtocol, Never>, input: Signal<TabloidCellViewModelProtocol, Never>.Observer) { get }
+    var elements: MutableProperty<[[TabloidCellViewModelProtocol]]> { get }
+    
+    var cellIdentifiers: [String] { get }
+    
+    // MARK: - Initialization
+    
+    init(cellIdentifiers: [String])
+}
+
 open class TabloidViewModel: TabloidViewModelProtocol {
     
     // MARK: - Properties
