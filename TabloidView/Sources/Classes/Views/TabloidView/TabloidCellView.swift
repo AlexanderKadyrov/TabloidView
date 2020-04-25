2475bf02ff669c0fd382dc240ee7cf938bb2195a
Alexander Kadyrov <alexander-kad@yandex.ru>
2020-04-26 01:40:19 +0300

    add TabloidCellView.swift

---
+++ TabloidView/Sources/Classes/Views/TabloidView/TabloidCellView.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+open class TabloidCellView: UITableViewCell, TabloidCellViewProtocol {
+    
+    // MARK: - Properties
+    
+    public var viewModel: TabloidCellViewModelProtocol!
+}
