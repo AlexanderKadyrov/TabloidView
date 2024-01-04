de2de843f75dbf4b2697fef8a6fdc82bd2585693
Alexander Kadyrov <a.kadyrov@alt.software>
2024-01-04 06:34:53 +0500

    fixes

--- TabloidView/Sources/Classes/Views/TabloidView/TabloidCellViewModel.swift
+++ TabloidView/Sources/Classes/Views/TabloidView/TabloidCellViewModel.swift
@@ -1,5 +1,7 @@
 import Foundation
+import Combine
 
 public protocol TabloidCellViewModel {
     var cellIdentifier: String { get set }
+    var didSelect: PassthroughSubject<TabloidCellViewModel, Never> { get set }
 }
