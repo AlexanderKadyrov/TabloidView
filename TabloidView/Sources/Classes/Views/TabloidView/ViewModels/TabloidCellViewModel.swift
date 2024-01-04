1d996c1ae7cd92975a6dca1e50802373ac72c2ab
Alexander Kadyrov <a.kadyrov@alt.software>
2024-01-04 05:59:41 +0500

    fixes

--- TabloidView/Sources/Classes/Views/TabloidView/ViewModels/TabloidCellViewModel.swift
+++ TabloidView/Sources/Classes/Views/TabloidView/ViewModels/TabloidCellViewModel.swift
@@ -2,11 +2,7 @@ import Foundation
 
 open class TabloidCellViewModel: NSObject {
     
-    // MARK: - Properties
-    
     public let cellIdentifier: String
-    
-    // MARK: - Initialization
     
     public init(cellIdentifier: String) {
         self.cellIdentifier = cellIdentifier
