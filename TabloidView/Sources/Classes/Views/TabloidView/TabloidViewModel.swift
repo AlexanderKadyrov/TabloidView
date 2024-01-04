020b62c7e996aa9a19553f633243af80bb3f2b98
Alexander Kadyrov <a.kadyrov@alt.software>
2024-01-04 06:09:40 +0500

    fixes

--- TabloidView/Sources/Classes/Views/TabloidView/TabloidViewModel.swift
+++ TabloidView/Sources/Classes/Views/TabloidView/TabloidViewModel.swift
@@ -1,11 +1,6 @@
 import Foundation
 import Combine
 
-public class TabloidViewModel {
-    
-    @Published var sections: [[TabloidCellViewModel]]
-    
-    public init() {
-        sections = []
-    }
+public protocol TabloidViewModelProtocol {
+    var sections: CurrentValueSubject<[[TabloidCellViewModel]], Never> { get set }
 }
