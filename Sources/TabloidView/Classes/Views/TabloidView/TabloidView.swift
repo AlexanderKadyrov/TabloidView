004c93c46593c0b0a1a1ba48cdb2e287d9b72e76
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-23 20:47:34 +0500

    fixes

--- Sources/TabloidView/Classes/Views/TabloidView/TabloidView.swift
+++ Sources/TabloidView/Classes/Views/TabloidView/TabloidView.swift
@@ -1,7 +1,7 @@
 import DifferenceKit
 import UIKit
 
-open class TabloidView: UITableView, UITableViewDelegate {
+open class TabloidView: UITableView, UITableViewDataSource, UITableViewDelegate {
     
     public var viewModel: TabloidViewModel? {
         didSet {
@@ -11,11 +11,13 @@ open class TabloidView: UITableView, UITableViewDelegate {
     
     public required init?(coder: NSCoder) {
         super.init(coder: coder)
+        dataSource = self
         delegate = self
     }
     
     public init(style: UITableView.Style) {
         super.init(frame: .zero, style: style)
+        dataSource = self
         delegate = self
     }
     
@@ -28,10 +30,48 @@ open class TabloidView: UITableView, UITableViewDelegate {
         }
     }
     
+    open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard
+            let cellViewModel = viewModel?.sections[indexPath.section].elements[indexPath.row],
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellViewModel.cellIdentifier, for: indexPath) as? TabloidCellView
+        else {
+            return UITableViewCell()
+        }
+        cell.cellViewModel = cellViewModel
+        return cell
+    }
+    
+    open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel?.sections[section].elements.count ?? .zero
+    }
+    
+    open func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel?.sections.count ?? .zero
+    }
+    
+    open func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return nil
+    }
+    
+    open func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return .zero
+    }
+    
+    open func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return nil
+    }
+    
+    open func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return .zero
+    }
+    
+    open func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
     open func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //guard let cellViewModel = viewModel(at: indexPath) else { return }
-        //cellViewModel.didSelect.send(cellViewModel)
-        //tableView.deselectRow(at: indexPath, animated: true)
+        guard let cellViewModel = viewModel?.sections[indexPath.section].elements[indexPath.row] else { return }
+        cellViewModel.didSelect(cellViewModel: cellViewModel)
     }
 }
 
