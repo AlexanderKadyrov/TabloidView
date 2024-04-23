6567f869d9fa01e6d48f4226e1c0d645c30b5a90
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-23 20:46:45 +0500

    fixes

--- Sources/TabloidView/Classes/Views/TabloidCellView/TabloidCellView.swift
+++ Sources/TabloidView/Classes/Views/TabloidCellView/TabloidCellView.swift
@@ -1,14 +1,5 @@
 import UIKit
 
 open class TabloidCellView: UITableViewCell {
-    
-    public var viewModel: TabloidCellViewModel? {
-        didSet {
-            fill(viewModel: viewModel)
-        }
-    }
-    
-    open func fill(viewModel: TabloidCellViewModel?) {
-        
-    }
+    open var cellViewModel: TabloidCellViewModel?
 }
