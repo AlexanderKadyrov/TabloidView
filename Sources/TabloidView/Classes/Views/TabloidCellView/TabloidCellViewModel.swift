ce5a0f9b156a89446d19da28df61e8e45a90c0e2
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-23 16:31:27 +0500

    fixes

--- Sources/TabloidView/Classes/Views/TabloidCellView/TabloidCellViewModel.swift
+++ Sources/TabloidView/Classes/Views/TabloidCellView/TabloidCellViewModel.swift
@@ -1,9 +1,7 @@
 import Foundation
-import Combine
 
 open class TabloidCellViewModel {
     
-    public let didSelect = PassthroughSubject<TabloidCellViewModel, Never>()
     public let cellIdentifier: String
     
     public init(cellIdentifier: String) {
