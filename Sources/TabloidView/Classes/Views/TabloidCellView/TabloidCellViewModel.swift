fd8d35d10384500584caee1501deae45b2b2d9c7
Alexander Kadyrov <a.kadyrov@alt.software>
2024-04-23 16:38:33 +0500

    fixes

--- Sources/TabloidView/Classes/Views/TabloidCellView/TabloidCellViewModel.swift
+++ Sources/TabloidView/Classes/Views/TabloidCellView/TabloidCellViewModel.swift
@@ -1,10 +1,19 @@
 import Foundation
+import DifferenceKit
 
-open class TabloidCellViewModel {
+open class TabloidCellViewModel: Differentiable {
+    
+    public var differenceIdentifier: String {
+        return ""
+    }
     
     public let cellIdentifier: String
     
     public init(cellIdentifier: String) {
         self.cellIdentifier = cellIdentifier
+    }
+    
+    public func isContentEqual(to source: TabloidCellViewModel) -> Bool {
+        return false
     }
 }
